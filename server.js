9c5eb671004e135ed5cd7ae7f8d1104cdff10415
Hend Aghil <hendmanga@gmail.com>
2020-05-09 17:36:11 +0200

    navbar, login, cards

--- server.js
+++ server.js
@@ -8,13 +8,16 @@ const mongoose = require("mongoose");
 mongoose.set('useUnifiedTopology', true);
 mongoose.set('useNewUrlParser', true);
 
+
 mongoose.connect(process.env.MONGO_CONNECTION_STRING)
 .then(()=>{console.log('Connected to MongoDB!')})
 .catch(()=>{console.error('MongoDB connection error!')});
 
 
+app.set('view engine', 'pug')
+app.use(express.static("public"))
 app.get("/", (req, res, next)=>{
-    res.send("hi")
+    res.render("index");
 });
 
 app.listen(process.env.PORT, ()=>{console.log("started!")});
