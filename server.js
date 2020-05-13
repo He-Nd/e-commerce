ceb2f9529f34ba31b5e8fe71ad95c237e9bbeea9
Hend Aghil <hendmanga@gmail.com>
2020-05-13 17:45:55 +0200

    register alerts, navbar part

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ require('dotenv').config()
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const productModel = require("./models/product");
+const bcrypt = require("bcryptjs");
+const Product = require("./models/product");
+const User = require("./models/user")
+
 
 
 mongoose.set('useUnifiedTopology', true);
@@ -21,6 +24,8 @@ mongoose.connect(process.env.MONGO_CONNECTION_STRING)
 
 app.set('view engine', 'pug')
 app.use(express.static("public"))
+app.use(express.urlencoded({extended: false}))
+
 app.get("/", async (req, res, next)=>{
 try{
     const products = await productModel.find()
@@ -34,8 +39,28 @@ catch(err){
 }
 });
 
-app.get("/register", (req, res, next)=>{
+app.get("/register",(req, res, next)=>{
     res.render("register");
 })
+
+app.post("/register",  async (req, res, next)=>{
+    // console.log(req.body)
+    
+    const username = req.body.username
+    const password = req.body.password
+    const encryptPassword = await bcrypt.hash(password, 10)
+    await new User({username, password: encryptPassword}).save();
+
+    res.send("okie <3");
+})
+
+app.get("/login", (req, res, next)=>{
+    res.render("login");
+})
+
+app.post("/login", (req, res, next)=>{
+    res.send("okie <3");
+})
+
 
 app.listen(process.env.PORT, ()=>{console.log("server started!")});
